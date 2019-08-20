b6b318c8c7afa66c14643b78cbb4ca71a7357de2
Jane Justiz <janejustiz@gmail.com>
2019-08-20 16:40:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -61,6 +61,7 @@ def can_be_found_updated_and_saved
   movie = Movie.create(title: "Awesome Flick")
   movie.title = "Even Awesomer Flick"
   movie.save
+  movie
 end
 
 def can_update_using_update_method
