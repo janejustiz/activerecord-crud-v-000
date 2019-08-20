42233c286da61c7b36c25b702e47493368beba92
Jane Justiz <janejustiz@gmail.com>
2019-08-20 16:33:50 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,9 +24,9 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(title: "Home Alone", release_date: 1990)
+
   Movie.create do |m|
-    title = m[title]
-    release_date = m[release_date]
+
   end
 end
 
